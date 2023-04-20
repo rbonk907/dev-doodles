5ab3a5f10ac2f34656accfccddb45fc97ce106c5
r.bonk907@gmail.com <r.bonk907@gmail.com>
2023-04-20 09:42:02 -0800

    connect to postgresql database

---
+++ dev-doodles-backend/db/index.js
@@ -0,0 +1,15 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    user: process.env.USER,
+    host: process.env.HOST,
+    database: process.env.DB,
+    password: process.env.PASSWORD,
+    port: process.env.PORT,
+});
+
+module.exports = {
+    query: (text, params, callback) => {
+        return pool.query(text, params, callback);
+    },
+}
