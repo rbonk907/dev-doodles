803717b8e5b357457c8fd6b4fb8c57376f8c1a4f
r.bonk907@gmail.com <r.bonk907@gmail.com>
2023-04-20 10:29:08 -0800

    add password authentication strategy

---
+++ dev-doodles-backend/routes/auth.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const crypto = require('crypto');
+const db = require('../db');
+
+/* create new strategy for handling username and password authentication.
+ * Query the database for a provided username and if it exists, determine if the
+ * hashed password provided matches the hashed password store in the DB.
+ */
+passport.use(new LocalStrategy(function verify(username, password, callback) {
+    db.query('SELECT * FROM users WHERE username = ?', [ username ], function(err, row) {
+        if (err) { return callback(err); }
+        if (!row) { return callback(null, false, { message: 'Incorrect username or password.' }); }
+
+        crypto.pbkdf2(password, row.salt, 310000, 32, 'sha256', function(err, hashedPassword) {
+            if (err) { return callback(err); }
+            if (!crypto.timingSafeEqual(row.hashed_password, hashedPassword)) {
+                return callback(null, false, { message: 'Incorrect username or password.' });
+            }
+            return callback(null, row);
+        });
+    });
+}));
