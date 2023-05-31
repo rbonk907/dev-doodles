a478b01ab65a055cce0ad354917a930bf9b11299
rbonk907 <r.bonk907@gmail.com>
2023-05-31 09:41:47 -0800

    style login form

--- dev-doodles-frontend/app/login/LoginForm.js
+++ dev-doodles-frontend/app/login/LoginForm.js
@@ -3,6 +3,7 @@
 import { useRouter } from "next/navigation";
 import { useContext, useEffect, useState } from "react";
 import { LoginContext } from "../LoginProvider";
+import { BsGoogle } from "react-icons/bs";
 
 export default function LoginForm() {
     const [username, setUsername] = useState("");
@@ -30,29 +31,44 @@ export default function LoginForm() {
             setAuth(true);
             router.push('/');
         }
-        // const jsonResponse = await response.json();
-        // console.log(jsonResponse);
+    }
+
+    function handleGoogleLogin() {
+        window.open("http://localhost:3001/login/federated/google", "_self");
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-                <label htmlFor="username">Username</label>
+        <form onSubmit={handleSubmit} className="flex flex-col justify-center items-center px-6 py-12 w-full md:w-3/4 max-w-xl h-auto md:rounded-2xl md:shadow-xl shadow-slate-300 bg-white">
+            <section className="mb-6 w-full max-w-md"> 
+                <label htmlFor="username" className="block font-bold">Username</label>
                 <input
                 type="text"
                 id="username"
                 name="username"
+                className="px-4 py-3 rounded-full w-full bg-gray-100" 
                 required
                 onChange={(e) => {setUsername(e.target.value)}}
                 ></input>
-                <label htmlFor="password">Password</label>
+            </section>
+            <section className="mb-6 w-full max-w-md">  
+                <label htmlFor="password" className="block font-bold">Password</label>
                 <input
                 type="password"
                 id="password"
                 name="password"
+                className="px-4 py-3 rounded-full w-full bg-gray-100" 
                 required
                 onChange={(e) => {setPassword(e.target.value)}}
                 ></input>
-                <button type="submit">Sign in</button>
+            </section> 
+            <button type="submit" className="px-6 py-4 mb-6 rounded-full w-full max-w-md bg-blue-500 text-white font-bold">Sign in</button>
+            <span className="w-full mb-6 border-b"></span>
+            <section className="w-full max-w-md">
+                <button onClick={handleGoogleLogin} className="grid grid-cols-[24px_minmax(0,_3fr)] justify-items-center px-6 py-4 rounded-full w-full max-w-md bg-red-500 text-white font-bold">
+                    <BsGoogle className="w-6 h-6 justify-self-start"/>
+                    <span className="justify-self-center w-full">Sign in with Google</span>
+                </button>
+            </section>
         </form>
     )
 }
--- dev-doodles-frontend/app/login/page.js
+++ dev-doodles-frontend/app/login/page.js
@@ -1,21 +1,14 @@
-"use client";
-
 import LoginProvider from "../LoginProvider";
 import LoginForm from "./LoginForm";
 
 export default function Page() {
-    
-    function handleGoogleLogin() {
-        window.open("http://localhost:3001/login/federated/google", "_self");
-    }
+
     
     return (
-        <div className="min-h-screen">
+        <div className="min-h-screen w-screen flex flex-col justify-center items-center bg-white md:bg-inherit">
             <LoginProvider>
                 <LoginForm />
             </LoginProvider>
-            <span className="block">or</span>
-            <button onClick={handleGoogleLogin}>Sign in with Google</button>
         </div>
     );
 }
