1b60b3f0782be4f91c44ecf6201c8a2e85e2f8b9
rbonk907 <r.bonk907@gmail.com>
2023-05-31 09:43:34 -0800

    edit pg-pool config based NODE_ENV

--- dev-doodles-backend/db/index.js
+++ dev-doodles-backend/db/index.js
@@ -1,13 +1,16 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-const pool = new Pool({
-    user: process.env.PG_USER,
-    host: process.env.HOST,
-    database: process.env.DB,
-    password: process.env.PASSWORD,
-    port: process.env.PORT,
-});
+const dbConfig = process.env.NODE_ENV === "production" ? { connectionString: process.env.PG_URI} 
+    : {
+        user: process.env.PG_USER,
+        host: process.env.HOST,
+        database: process.env.DB,
+        password: process.env.PASSWORD,
+        port: process.env.PORT,
+    };
+
+const pool = new Pool(dbConfig);
 
 module.exports = {
     query: (text, params, callback) => {
