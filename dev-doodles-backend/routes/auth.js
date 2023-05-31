ce66cd7a418873d29a93af9bc9c5b4d7bf9a1c51
rbonk907 <r.bonk907@gmail.com>
2023-05-31 13:02:21 -0800

    hardcode google callback URL

--- dev-doodles-backend/routes/auth.js
+++ dev-doodles-backend/routes/auth.js
@@ -32,7 +32,7 @@ passport.use(new LocalStrategy(function verify(username, password, callback) {
 passport.use(new GoogleStrategy({
     clientID: process.env['GOOGLE_CLIENT_ID'],
     clientSecret: process.env['GOOGLE_CLIENT_SECRET'],
-    callbackURL: '/oauth2/redirect/google',
+    callbackURL: 'https://backend-dev-doodles.onrender.com/oauth2/redirect/google',
     scope: [ 'profile' ],
     state: true
 },
